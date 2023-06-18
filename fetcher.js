818b4584e623d5aab39cfa2c7cb7f041c5d7f21e
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-18 15:04:00 -0400

    build request

--- fetcher.js
+++ fetcher.js
@@ -3,15 +3,23 @@
 
 // This program will take a URL and a local file path as command line arguments, then download the resource to the specified path.
 
-// assign variables to required modules 
-const request = require('request');
-const fs =require('fs');
+// assign variables to required modules
+const request = require("request");
+const fs = require("fs");
 
 // assign variables to command line arguments
-const [ url, path ]  = process.argv.slice(2);
-console.log(url, path);
+const [url, fileName] = process.argv.slice(2);
+const path = `./fetchedData/${fileName}`;
 
 
-
-
- 
+//download the resource from the URL and save it to the specified path
+request(url, (error, response, body) => {
+  if (error) console.log("request error:", error); // Print the error if one occurred
+  if (response.statusCode < 202) {
+    console.log(body); // Print the HTML for given URL.
+    fs.writeFile(path, body, (err) => {
+      if (err) console.log("Write error:", err);
+      console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+    });
+  }
+});
